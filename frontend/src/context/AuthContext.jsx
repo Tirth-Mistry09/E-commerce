5340e7158181a55cd37fb8741d0c914edd6d007b
Tirth-Coder <mistrytirth0905@gmail.com>
2026-06-12 16:34:32 +0530

    fix api address

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -26,7 +26,7 @@ export const AuthProvider = ({ children }) => {
     
     try {
       const response = await axios.post(
-        `${import.meta.env.VITE_API_URL}/auth/signup`,
+`${import.meta.env.VITE_API_URL}/api/auth/signup`,
         { username, email, password }
       );
       
@@ -53,7 +53,7 @@ export const AuthProvider = ({ children }) => {
     
     try {
       const response = await axios.post(
-        `${import.meta.env.VITE_API_URL}/auth/login`,
+`${import.meta.env.VITE_API_URL}/api/auth/login`,
         { email, password }
       );
       
